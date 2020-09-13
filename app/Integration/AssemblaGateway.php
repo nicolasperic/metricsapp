87cda849058d68fd91d9715e6d9641e58f587544
Nicolas Peric <nperic@summasolutions.net>
2020-09-13 11:03:39 -0300

    AssemblaGateway: added API documentation and links

--- app/Integration/AssemblaGateway.php
+++ app/Integration/AssemblaGateway.php
@@ -9,27 +9,121 @@ class AssemblaGateway
     /** relationship is 5 - ticket2 is story and ticket1 is subtask of the story */
     const STORY_RELATION = 5;
 
+    /**
+     * Returns currently authenticated user.
+     * https://api-docs.assembla.cc/content/ref/user_show.html
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function getAuthenticatedUser()
     {
         return AssemblaRequest::get('user');
     }
 
+    /**
+     * Get list of spaces user is participating to
+     * https://api-docs.assembla.cc/content/ref/spaces_index.html
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function getSpaces()
     {
         return AssemblaRequest::get('spaces');
     }
 
+    /**
+     * Returns users for a specified space.
+     * https://api-docs.assembla.cc/content/ref/space_users.html
+     *
+     * @param $space
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function getSpaceUsers($space)
     {
         return AssemblaRequest::get("spaces/{$space}/users");
     }
 
+    /**
+     * Returns a ticket by a ticket number.
+     * https://api-docs.assembla.cc/content/ref/tickets_show.html
+     *
+     * @param $space
+     * @param $ticketNumber
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function getTicketBySpaceAndNumber($space, $ticketNumber)
     {
         return AssemblaRequest::get("spaces/{$space}/tickets/{$ticketNumber}");
 
     }
 
+    /**
+     * Returns a list of ticket associations
+     * https://api-docs.assembla.cc/content/ref/ticket_associations_index.html
+     *
+     * @param $space
+     * @param $ticketNumber
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getTicketAssociationsBySpaceAndNumber($space, $ticketNumber)
+    {
+        return AssemblaRequest::get("spaces/{$space}/tickets/{$ticketNumber}/ticket_associations");
+    }
+
+    /**
+     * Get the list of tickets for a milestone
+     * https://api-docs.assembla.cc/content/ref/milestone_tickets.html
+     *
+     * @param $space
+     * @param $milestoneId
+     * @param $queryParams array
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getTicketsForMilestone($space, $milestoneId, $queryParams = [])
+    {
+        return AssemblaRequest::get("spaces/{$space}/tickets/milestone/{$milestoneId}", $queryParams);
+    }
+
+    /**
+     * Returns a paginated list of tasks. Pages are default to 25 tasks.
+     * https://api-docs.assembla.cc/content/ref/tasks_index.html
+     *
+     * @param $queryParams
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getTrackedTimeForTicket($queryParams)
+    {
+        return AssemblaRequest::get("tasks", $queryParams);
+    }
+
+    /**
+     * Returns a list of paginated upcoming milestones. Pages are defaulted to 10 milestones.
+     * https://api-docs.assembla.cc/content/ref/milestones_index.html
+     *
+     * @param $space
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getMilestonesForSpace($space)
+    {
+        return AssemblaRequest::get("spaces/{$space}/milestones");
+    }
+
+    /**
+     * This function is used to validate if a ticket exists on a given space. If set $validateData array will allow to
+     * us to also validate any ticket field i.e if is a subtask ['is_story' => false] or a story  ['is_story' => true]
+     *
+     * @param      $space
+     * @param      $ticketNumber
+     * @param null $validateData
+     *
+     * @return bool|mixed
+     */
     public function validateTicketExistsBySpaceAndNumber($space, $ticketNumber, $validateData = null)
     {
         try {
@@ -50,37 +144,5 @@ class AssemblaGateway
         } catch (ClientException $exception) {
             return false;
         }
-    }
-
-    public function getTicketAssociationsBySpaceAndNumber($space, $ticketNumber)
-    {
-        return AssemblaRequest::get("spaces/{$space}/tickets/{$ticketNumber}/ticket_associations");
-    }
-
-    /**
-     * @param $space
-     * @param $milestoneId
-     * @param $queryParams array
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function getTicketsForMilestone($space, $milestoneId, $queryParams = [])
-    {
-        return AssemblaRequest::get("spaces/{$space}/tickets/milestone/{$milestoneId}", $queryParams);
-    }
-
-    /**
-     * @param $queryParams
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function getTrackedTimeForTicket($queryParams)
-    {
-        return AssemblaRequest::get("tasks", $queryParams);
-    }
-
-    public function getMilestonesForSpace($space)
-    {
-        return AssemblaRequest::get("spaces/{$space}/milestones");
     }
 }
