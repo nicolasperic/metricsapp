a579e35f0a94c5e2b00fb2a5ac9740513e33a16d
Nicolas Peric <nperic@summasolutions.net>
2021-01-09 23:28:33 -0300

    Styles updates for current sprints page

--- resources/views/sprints/current.blade.php
+++ resources/views/sprints/current.blade.php
@@ -7,48 +7,138 @@
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">Your Current Sprints</div>
 
-                        <ul>
-                            <?php $totalEstimate = 0;$totalRemainingEstimate = 0;$totalCompletedEstimate = 0;?>
-                            @forelse ($currentSprints as $sprint)
-                                <li>
-                                    @foreach ($sprint->projects as $project)
-                                        <a href="{{url("projects/{$project->id}")}}">{{$project->name}}</a>
-                                    @endforeach
-                                    > <a href="{{url("sprints/{$sprint->id}")}}">{{ $sprint->name}}</a>  <?= $sprint->getFormattedPlannerType()?>
-                                        - Worked Hours {{ $sprint->getTotalWorkedHours() }}
-                                        - Remaining Hours {{ $sprint->getTotalWorkingHours() }}
-                                        - Remaining Estimate {{ $sprintRemainingEstimate = $sprint->getTotalRemainingEstimate() }}
-                                        - Completed Estimate: {{ $sprintCompletedEstimate = $sprint->getTotalCompletedEstimate() }} ({{ $sprint->getTotalCompletedEstimatePercentage() }}%)
-                                        - Total Estimate: {{ $sprintTotalEstimate = $sprint->getTotalEstimate() }}
-                                        <!-- - Total Stories {{ $sprint->getTotalStories() }}
-                                        - Completed Stories {{ $sprint->getCompletedStories() }} -->
-                                    <?php
-                                        $totalRemainingEstimate += $sprintRemainingEstimate;
-                                        $totalCompletedEstimate += $sprintCompletedEstimate;
-                                        $totalEstimate += $sprintTotalEstimate;
-                                     ?>
-                                </li>
-
-
-                            @empty
-                                <p>No currents sprints created yet</p>
-                            @endforelse
-                        </ul>
-
-                    <!-- Estimate may vary on each space > Story Points or Hours -->
-                    <span>Total Remaining Estimate: <?= $totalRemainingEstimate?></span>
-                    <span>Total Completed Estimate: <?= $totalCompletedEstimate?></span>
-                    <span>Total Estimate: <?= $totalEstimate?></span>
-                </div>
+            <?php $totalEstimate = 0;$totalRemainingEstimate = 0;$totalCompletedEstimate = 0; $totalWorkedHours = 0; $totalRemainingHours = 0; $totalStories = 0;?>
+            @forelse ($currentSprints as $sprint)
+            <div class="col-10" style="margin-bottom:20px">
+                <div>
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center ">
+                            @foreach ($sprint->projects as $project)
+                            <a href="{{url("projects/{$project->id}")}}">{{$project->name}}</a>
+                            @endforeach
+                            &nbsp;>&nbsp; <a href="{{url("sprints/{$sprint->id}")}}">{{ $sprint->name}}</a>  &nbsp;<?= $sprint->getFormattedPlannerType()?>
+                            &nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://app.assembla.com/spaces/{{ $project->wikiname }}/milestones/{{ $sprint->sprint_assembla_id }}" target="_blank">View in Assembla</a>
+                        </div>
+                        <div class="card-bg-secondary">
+                            <div class="d-flex">
+                                <div class="w-33 border-right border-bottom">
+                                    <div class="p-4">
+                                        <small class="text-uppercase">Worked Hours</small>
+                                        <h4 class="mt-4 mb-0">{{ $sprintTotalWorkedHours = $sprint->getTotalWorkedHours() }}</h4>
+                                    </div>
+                                </div>
+                                <div class="w-33 border-right border-bottom">
+                                    <div class="p-4">
+                                        <small class="text-uppercase">Remaining Hours</small>
+                                        <h4 class="mt-4 mb-0">{{ $sprintTotalRemainingHours = $sprint->getTotalWorkingHours() }}</h4>
+                                    </div>
+                                </div>
+                                <div class="w-33 border-bottom">
+                                    <div class="p-4">
+                                        <small class="text-uppercase">Stories</small>
+                                        <h4 class="mt-4 mb-0">{{ $sprintTotalStories = $sprint->getTotalStories() }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex">
+                                <div class="w-33 border-right">
+                                    <div class="p-4 mb-0">
+                                        <small class="text-uppercase">Remaining Estimates</small>
+                                        <h4 class="mt-4">{{ $sprintRemainingEstimate = $sprint->getTotalRemainingEstimate() }} ({{ ($sprint->getTotalCompletedEstimatePercentage() != 0)?100 - $sprint->getTotalCompletedEstimatePercentage():0 }}%)</h4>
+                                    </div>
+                                </div>
+                                <div class="w-33 border-right">
+                                    <div class="p-4 mb-0">
+                                        <small class="text-uppercase">Completed Estimates</small>
+                                        <h4 class="mt-4">{{ $sprintCompletedEstimate = $sprint->getTotalCompletedEstimate() }} ({{ $sprint->getTotalCompletedEstimatePercentage() }}%)</h4>
+                                    </div>
+                                </div>
+                                <div class="w-33">
+                                    <div class="p-4 mb-0">
+                                        <small class="text-uppercase">Total Estimates</small>
+                                        <h4 class="mt-4">{{ $sprintTotalEstimate = $sprint->getTotalEstimate() }}</h4></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!----> </div>
             </div>
+
+            <?php
+                $totalRemainingEstimate += $sprintRemainingEstimate;
+                $totalCompletedEstimate += $sprintCompletedEstimate;
+                $totalEstimate          += $sprintTotalEstimate;
+                $totalWorkedHours       += $sprintTotalWorkedHours;
+                $totalRemainingHours    += $sprintTotalRemainingHours;
+                $totalStories           += $sprintTotalStories;
+            ?>
+            @empty
+                <p>No currents sprints created yet</p>
+            @endforelse
+
+            @if(count($currentSprints) > 1)
+                <?php $totalCompletedEstimatePercentage = ($totalEstimate != 0)? number_format($totalCompletedEstimate/$totalEstimate*100,2):0?>
+                <div class="col-10" style="margin-bottom:20px">
+                    <div>
+                        <div class="card">
+                            <div class="card-header d-flex align-items-center ">
+                                <h5>Total</h5>
+                            </div>
+                            <div class="card-bg-secondary">
+                                <div class="d-flex">
+                                    <div class="w-33 border-right border-bottom">
+                                        <div class="p-4">
+                                            <small class="text-uppercase">Total Worked Hours</small>
+                                            <h4 class="mt-4 mb-0">{{ $totalWorkedHours }}</h4>
+                                        </div>
+                                    </div>
+                                    <div class="w-33 border-right border-bottom">
+                                        <div class="p-4">
+                                            <small class="text-uppercase">Total Remaining Hours</small>
+                                            <h4 class="mt-4 mb-0">{{ $totalRemainingHours }}</h4>
+                                        </div>
+                                    </div>
+                                    <div class="w-33 border-bottom">
+                                        <div class="p-4">
+                                            <small class="text-uppercase">Total Stories</small>
+                                            <h4 class="mt-4 mb-0">{{ $totalStories }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex">
+                                    <div class="w-33 border-right">
+                                        <div class="p-4 mb-0">
+                                            <small class="text-uppercase">Total Remaining Estimates</small>
+                                            <h4 class="mt-4">{{ $totalRemainingEstimate }} ({{ 100 - $totalCompletedEstimatePercentage }}%)</h4>
+                                        </div>
+                                    </div>
+                                    <div class="w-33 border-right">
+                                        <div class="p-4 mb-0">
+                                            <small class="text-uppercase">Total Completed Estimates</small>
+                                            <h4 class="mt-4">{{ $totalCompletedEstimate }} ({{ $totalCompletedEstimatePercentage }}%)</h4>
+                                        </div>
+                                    </div>
+                                    <div class="w-33">
+                                        <div class="p-4 mb-0">
+                                            <small class="text-uppercase">Total Estimates</small>
+                                            <h4 class="mt-4">{{ $totalEstimate }}</h4></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!----> </div>
+                </div>
+            @endif
+
+
         </div>
     </div>
     <style>
         <?php //TODO move styles to CSS/SCSS?>
+        .w-33 {
+            width: 34% !important;
+        }
         .planner-type {
             padding: 3px 5px 3px;
             border-radius: 3px;
